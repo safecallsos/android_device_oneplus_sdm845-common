c7d4dbf90ade42c5a1ee8d28d6271f8a75fdbb57
firebird11 <hbgassel@gmail.com>
2019-01-02 09:31:57 +0100

    sdm845-common: add cne feature
    Change-Id: I07641b1c65d87b673def10a58530dd68f873fcff

--- system_prop.mk
+++ system_prop.mk
@@ -26,6 +26,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=true
 
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.gfx_ubwc_disable=0 \
